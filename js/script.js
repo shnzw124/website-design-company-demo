538b4c44c356510efdb258d5050d251d9809c62e
新澤耕平 <koheishinsawa@Ko-Mac.local>
2021-09-27 19:11:48 +0900

    スムーススクロール実装

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
 $(function(){
   // ハンバーガーメニュー
-  $('.burger__icon').on('click',function(){
-    $('.burger__icon').toggleClass('close');
-    $('.burger__wrapper').fadeToggle(500);
-  });
-
-  $('.burger__item').on('click',function(){
+  $('.burger__icon, .burger__item').on('click',function(){
     $('.burger__icon').toggleClass('close');
     $('.burger__wrapper').fadeToggle(500);
   });
@@ -20,4 +15,13 @@ $(function(){
       $("#header").css("position", "static");
     }
   });
+
+  // スムーススクロール
+  $('.header__link, .header__btn > .btn').click(function(){
+    var target = $($(this).attr('href')).offset().top;
+
+    $('html, body').animate({scrollTop: target - 70}, 500);
+    return false;
+  });
+
 });
