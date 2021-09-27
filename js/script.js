428d83ca9a1be7932214067099e84e232909fd09
新澤耕平 <koheishinsawa@Ko-Mac.local>
2021-09-27 18:34:04 +0900

    ハンバーガーメニュー実装

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(function(){
+  // ハンバーガーメニュー
+  $('.burger__icon').on('click',function(){
+    $('.burger__icon').toggleClass('close');
+    $('.burger__wrapper').fadeToggle(500);
+  });
 
+  $('.burger__item').on('click',function(){
+    $('.burger__icon').toggleClass('close');
+    $('.burger__wrapper').fadeToggle(500);
+  });
+  
 });
