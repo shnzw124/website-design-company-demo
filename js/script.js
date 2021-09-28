c3675d241334ea5630c278e9dc3dafc18f71311f
新澤耕平 <koheishinsawa@Ko-Mac.local>
2021-09-28 10:05:21 +0900

    スライダー実装

--- js/script.js
+++ js/script.js
@@ -48,4 +48,37 @@ $(function(){
     }
   });
 
+  // スライダー
+  var swiper = new Swiper('.swiper-container', {
+    // デフォルト設定
+    slidesPerView: 5,
+    spaceBetween: 50,
+    // ブレーポイント（画面幅による設定）
+    breakpoints: {
+      // 画面幅が 414px 以下の場合（window width =< 414px）
+      414: {
+        slidesPerView: 1,
+        spaceBetween: 10
+      },
+      // 画面幅が 1024px 以下の場合（window width =< 1024px）
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
 });
